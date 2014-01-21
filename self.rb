88d16c02406a154f667248860875b84441ebbe57
Morton Fox <github@mortonfox.otherinbox.com>
2014-01-21 15:07:09 -0500

    Added author line.

--- self.rb
+++ self.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 
+# Script for moving my own photos in and out of views groups so that those follow group criteria.
+# Author: Po Shan Cheah http://mortonfox.com
+
+
 unless Kernel.respond_to? :require_relative
   # Add require_relative shim for Ruby 1.8 compatibility.
   module Kernel
