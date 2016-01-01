048167d43bd561d89c1e42788b77bf86dbf7dd11
Morton Fox <github@qslw.com>
2016-01-01 01:09:48 -0500

    Changes suggested by Rubocop.

--- pool.rb
+++ pool.rb
@@ -18,20 +18,18 @@ require_relative 'log'
 require 'optparse'
 
 module PoolRB
-
   # Clean Flickr views groups that we manage.
   class CleanPool
-
     SERVICE_NAME = 'pool'
 
     PAGELEN = 100
 
     GROUPS = {
-      25 => { :name => '1-25 Views', :id => '66969363@N00', :range => 0..25 },
-      50 => { :name => '25-50 Views', :id => '55265535@N00', :range => 25..50 },
-      75 => { :name => '50-75 Views', :id => '38541060@N00', :range => 50..75 },
-      100 => { :name => '75-100 Views', :id => '45499242@N00', :range => 75..100 },
-      200 => { :name => '150-200 Views', :id => '57008537@N00', :range => 150..200 },
+      25 => { name: '1-25 Views', id: '66969363@N00', range: 0..25 },
+      50 => { name: '25-50 Views', id: '55265535@N00', range: 25..50 },
+      75 => { name: '50-75 Views', id: '38541060@N00', range: 50..75 },
+      100 => { name: '75-100 Views', id: '45499242@N00', range: 75..100 },
+      200 => { name: '150-200 Views', id: '57008537@N00', range: 150..200 },
     }
 
     def initialize testmode = false
@@ -51,7 +49,7 @@ module PoolRB
 
     def get_photos groupid, pagenum
       Flickr.flickr_retry {
-        flickr.groups.pools.getPhotos :group_id => groupid, :per_page => PAGELEN, :page => pagenum, :extras => 'views'
+        flickr.groups.pools.getPhotos group_id: groupid, per_page: PAGELEN, page: pagenum, extras: 'views'
       }
     end
 
@@ -65,23 +63,23 @@ module PoolRB
       i = 0
       photos.each { |photo|
         views = photo.views.to_i
-        unless range.cover?(views)
-          i += 1
-          url = "http://www.flickr.com/photos/#{photo.owner}/#{photo.id}"
-          @log.puts "#{i}. <a href=\"#{url}\">#{photo.id}</a>: #{photo.title} by #{photo.ownername}, <b>#{views}</b> views"
-          puts "#{i}. Rejecting photo #{photo.id} (#{views} views) ..."
+        next if range.cover?(views)
 
-          next if @testmode
+        i += 1
+        url = "http://www.flickr.com/photos/#{photo.owner}/#{photo.id}"
+        @log.puts "#{i}. <a href=\"#{url}\">#{photo.id}</a>: #{photo.title} by #{photo.ownername}, <b>#{views}</b> views"
+        puts "#{i}. Rejecting photo #{photo.id} (#{views} views) ..."
 
-          begin
-            Flickr.flickr_retry {
-              flickr.groups.pools.remove :group_id => groupid, :photo_id => photo.id
-            }
-            sleep 0.5
-          rescue FlickRaw::FailedResponse => err
-            warn "Failed to remove photo #{photo.id}: #{err.code} #{err.msg}"
-            @log.puts "Failed to remove photo #{photo.id}: #{err.code} #{err.msg}"
-          end
+        next if @testmode
+
+        begin
+          Flickr.flickr_retry {
+            flickr.groups.pools.remove group_id: groupid, photo_id: photo.id
+          }
+          sleep 0.5
+        rescue FlickRaw::FailedResponse => err
+          warn "Failed to remove photo #{photo.id}: #{err.code} #{err.msg}"
+          @log.puts "Failed to remove photo #{photo.id}: #{err.code} #{err.msg}"
         end
       }
     end
@@ -124,7 +122,7 @@ module PoolRB
         pagetotals[gkey] = result.pages
         totalpages += result.pages
       }
-      [ totalpages, pagetotals ]
+      [totalpages, pagetotals]
     end
 
     # Clean the pages of each group in order from most recent to oldest.
